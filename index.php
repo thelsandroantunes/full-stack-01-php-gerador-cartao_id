18d6c88551425443ff3cd243119199d1a89643c5
Thelsandro Antunes <thelsandro.developer@gmail.com>
2022-05-06 14:49:17 -0400

    update

--- index.php
+++ index.php
@@ -47,7 +47,40 @@
     </div>
     <!-- ===== Edit Modal End ===== -->
     <!-- ===== Navigation Bar Start ===== -->
-    
+    <nav class="navbar navbar-expand-lg navbar-dark">
+        <a href="" class="navbar-brand"><img src="" alt=""></a>
+        <button class="navbar-toggler">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Dropdown
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Ação</a>
+                <a class="dropdown-item" href="#">Nova ação</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Outra aqui</a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Desabilitar</a>
+            </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Digite aqui..." aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
+            </form>
+        </div>        
+    </nav>
     <!-- ===== Navigation Bar End ===== -->
 </body>
 </html>
